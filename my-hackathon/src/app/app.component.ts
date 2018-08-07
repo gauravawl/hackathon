9167314ec09579171faf7a6714e0dce0766c00e5
Gourav Agrawal <gourav.agrawal@incedoinc.com>
2018-08-07 15:25:04 +0530

    Changes to UI

--- my-hackathon/src/app/app.component.ts
+++ my-hackathon/src/app/app.component.ts
@@ -7,8 +7,11 @@ import { Response } from '@angular/http/src/static_response';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  outputFlag = false;
   title = 'Fake news Detection';
-
+  result = '';
+  probability = '';
+  high = [];
   input = {
     newsText: ""
   };
@@ -19,6 +22,15 @@ export class AppComponent {
   search() {
     this.newsService.getResult(this.input.newsText).subscribe(response => {
       console.log(response);
+      this.result = response["RESULT"][0][0];
+      this.probability = response["RESULT"][0][0][0];
+
+      if (this.result) {
+        this.outputFlag = true;
+      }
+
+      this.high = response["HIGH"];
+
 
     });
   }
