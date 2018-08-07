aa1a953769cac2d39ec26c30f8fa4e674c263d03
Gaurav Soni <gsoni@incedoinc.com>
2018-08-07 22:17:08 +0530

    Added more tags, google api key, handled description field

--- source_checker/source_checker.py
+++ source_checker/source_checker.py
@@ -28,9 +28,9 @@ class SourceChecker(object):
         self.text = text
         self.language = language
         self.cat_dict = defaultdict(list)
-        key = ''
+        key = 'AIzaSyAWd8DRmyj_feeOl7_48ml9Bm0jpRS-1Ig'
         self.engine = Google(license=key, throttle=0.5, language=None)
-        self.cat_weightage_dict = {'imposter site' : 0.2 , 'fake news' : 0.0 , 'parody site' : 0.0 , 'some fake stories' : 0.0 , 'conspiracy' : 0.4 , 'fake' : 0.0 , 'rumor' : 0.2 , 'unreliable' : 0.3 , 'reliable' : 0.9 , 'bias' : 0.7 , 'clickbait' : 0.3 , 'satire' : 0.0 , 'junksci' : 0.3 , 'political' : 0.8 , 'hate' : 0.3 , 'blog' : 0.5 , 'satirical' : 0.1 , 'unrealiable' : 0.5 , 'questionable' : 0.4 , 'least_biased' : 1.0 , 'pseudoscience' : 0.5 , 'right_center' : 0.8 , 'pro_science' : 0.8 , 'left_center' : 0.8 , 'right' : 0.8 , 'left' : 0.8}
+        self.cat_weightage_dict = {'imposter site' : 0.2 , 'fake news' : 0.0 , 'parody site' : 0.0 , 'some fake stories' : 0.0 , 'conspiracy' : 0.4 , 'fake' : 0.0 , 'rumor' : 0.2 , 'unreliable' : 0.3 , 'reliable' : 0.9 , 'bias' : 0.7 , 'clickbait' : 0.3 , 'satire' : 0.0 , 'junksci' : 0.3 , 'political' : 0.8 , 'hate' : 0.3 , 'blog' : 0.5 , 'satirical' : 0.1 , 'unrealiable' : 0.3 , 'questionable' : 0.4 , 'least_biased' : 1.0 , 'pseudoscience' : 0.5 , 'right_center' : 0.8 , 'pro_science' : 0.8 , 'left_center' : 0.8 , 'right' : 0.8 , 'left' : 0.8, 'biased' : 0.8, 'state' : 0.5}
 
     def get_queries(self):
 
@@ -57,6 +57,7 @@ class SourceChecker(object):
         else:
             stop_words = []
         queries = []
+        queries.append(self.text)
         for ngram in all_ngrams:
             num_stop = len([w for w in ngram if w in stop_words])
             stop_score = float(num_stop)/len(ngram)
@@ -100,7 +101,10 @@ class SourceChecker(object):
         domain_file = Datasheet.load(sources_path, headers = True)
         for row in domain_file:
             url  = row[1]
-            cats = row[2:]
+            if str(row[-1]).find("\""):
+                cats = row[2:-1]
+            else:
+                cats = row[2:]
             self.cat_dict[url] = cats
 
     def pairwise(self, t):
@@ -148,16 +152,16 @@ class SourceChecker(object):
             len2 = 0
             if output[deg]:
                 for d, cats in sorted(output[deg]):
+                    score3 = 0.0
+                    len2 += 1
+                    len3 = len(cats)
                     if cats:
-                        score3 = 0.0
-                        len2 += 1
-                        len3 = len(cats)
                         for cat in cats:
                             score3 += self.cat_weightage_dict[cat]
                         score3 /= len3
-                        score2 += score3
-                    else:
-                        continue
+                    elif score3 == 0.0:
+                        score3 = 0.5
+                    score2 += score3
             if len2 != 0:
                 score2 /= len2
             score1[deg] = score2
